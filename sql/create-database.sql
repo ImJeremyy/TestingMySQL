43101c8b852b82486a738ec38ef23bd76994e8d3
ImJeremyy <madivoso@gmail.com>
2019-12-07 13:38:27 -0500

    Added more to players and accoutns table

--- sql/create-database.sql
+++ sql/create-database.sql
@@ -3,12 +3,15 @@ CREATE DATABASE `sql_economy`;
 USE `sql_economy`;
 
 CREATE TABLE `players` (
-	`uuid` VARCHAR(16) NOT NULL PRIMARY KEY AUTO_INCREMENT, -- unique user id (player.getUniqueId().toString())
-    `name` VARCHAR(16) NOT NULL
+	`uuid` VARCHAR(16) NOT NULL PRIMARY KEY, -- unique user id (player.getUniqueId().toString())
+	`name` VARCHAR(16) NOT NULL,
+	`joined_timestamp` TIMESTAMP NOT NULL DEFAULT(CURRENT_TIMESTAMP),
+	`bio` VARCHAR(64) NULL
 	);
     
 CREATE TABLE `accounts` (
-	`uuid` VARCHAR(16) NOT NULL PRIMARY KEY, -- owner
-    `balance` DECIMAL NOT NULL
+	`account_id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`uuid` VARCHAR(16) NOT NULL, -- owner
+	`balance` DECIMAL NOT NULL DEFAULT(0)
 	);
     
